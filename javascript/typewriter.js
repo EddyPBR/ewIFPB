8a2e96a5406a795ae2b4bf723a4d11ef34e3f7b1
EddyPBRm <edvaldojunior1310@gmail.com>
2019-08-29 21:12:32 -0300

    edited header, added +1 profile of organization

--- javascript/typewriter.js
+++ javascript/typewriter.js
@@ -3,9 +3,17 @@ function typewriter(element) {
   let letters = element.innerHTML.split('');
   element.innerHTML = ''; 
   
-  letters.forEach(( letter, index, letters ) => {
+  letters.forEach(( letter, index ) => {
     setTimeout(() => element.innerHTML += letter, 100 * index);
   });
+
+  
+  setTimeout( function() {
+    var img = document.querySelector('.ifpb-img');
+    img.classList.remove('is-hidden');
+    img.classList.remove('animated');
+    img.classList.add('animated');
+  }, 6500);
 }
 
 const text = document.querySelector('.is-typewriter');
